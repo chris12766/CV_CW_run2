88fa21b818e19ba2b4d739e50ea8194a7b5b66b3
Christian <chkaradjov@abv.bg>
2018-11-20 11:58:24 +0000

    first commit

---
+++ SceneRecognition_Run2/src/main/java/chk1g16_ty1g16/App.java
@@ -0,0 +1,15 @@
+package chk1g16_ty1g16;
+
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.colour.ColourSpace;
+import org.openimaj.image.colour.RGBColour;
+import org.openimaj.image.processing.convolution.FGaussianConvolve;
+import org.openimaj.image.typography.hershey.HersheyFont;
+
+public class App {
+
+    public static void main( String[] args ) {
+                
+    }
+}
